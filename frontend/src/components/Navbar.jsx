b2ee48dfc2526f3e06bb71e1865d75ccccf2a7e1
Delunsss2345 <huypham1459@gmail.com>
2025-06-06 17:45:46 +0700

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { ClipboardList } from 'lucide-react';
 import { Link } from 'react-router-dom';
 const NavBar = () => {
-    return <nav className=" hidden lg:flex flex-col w-[15%] bg-nav transition-colors duration-300 dark:text-white dark:bg-dark-mode h-screen p-2 lg:p-4 xl:p-6">
+    return <nav className="hidden lg:flex flex-col w-[15%] bg-nav transition-colors duration-300 dark:text-white dark:bg-dark-mode h-screen p-2 lg:p-4 xl:p-6">
+
         <div className="flex items-center gap-x-2">
             <figure className='w-12'>
-                <img src="../src/images/logo.png" alt="Logo" />
+                <img src="/src/images/logo.png" alt="Logo" />
             </figure>
             <h1 className="font-semibold text-sm lg:text-xl">HealDocs</h1>
         </div>
