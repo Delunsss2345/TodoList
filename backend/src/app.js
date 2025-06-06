555cd2049a6597f13524ab55671e1ea7976e1d2c
Delunsss2345 <huypham1459@gmail.com>
2025-06-06 17:04:22 +0700

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParse from 'cookie-parser'
 import cors from 'cors' 
+import path from "path";
 import {connectDB} from '../lib/db.js'
 import authRoutes from '../routes/user.route.js'
 import todoRoutes from '../routes/todo.route.js'
@@ -13,11 +14,17 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true, // cookie / token
 }));
-
+const __dirname = path.resolve();
 app.use("/api/auth", authRoutes) ;
 app.use("/api" , todoRoutes)
 const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(port , (req ,res) => {
     connectDB() ; 
     console.log(`Listening http://localhost:${port}`) ; 
